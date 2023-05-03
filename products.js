b732aa42b56a9363cfaa41d8086a45e96789acdd
Julian Mesa <judmesabu@gmail.com>
2023-05-03 14:03:55 -0500

    Primer commit

---
+++ products.js
@@ -0,0 +1,10 @@
+class Products {
+    constructor(id, name, price, stock, category, posterUrl) { 
+        this.id = id
+        this.name = name
+        this.price = price
+        this.stock = stock
+        this.category = category
+        this.posterUrl = posterUrl
+    }
+}
